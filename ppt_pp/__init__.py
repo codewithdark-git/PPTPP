9c6e1fa14cda3b7435db19864834cb0cbe2ca885
codewithdark-git <codewithdark90@gmail.com>
2026-05-01 21:40:04 +0500

    Initialize PPT++ module with versioning and author information

---
+++ ppt_pp/__init__.py
@@ -0,0 +1,8 @@
+"""
+PPT++: Enhanced Token Pruning & Pooling for Efficient Vision Transformers
+=========================================================================
+Modular codebase supporting DeiT and LV-ViT backbones.
+"""
+
+__version__ = "1.0.0"
+__author__ = "PPT++ Research Team"
